0171af456f92fc8822396cb61bdc860fa60f246e
Max <max@lythium.dev>
2024-04-28 22:42:58 +0100

    Update sh_init.lua

--- lua/pulsar_lib/sh_init.lua
+++ lua/pulsar_lib/sh_init.lua
@@ -13,10 +13,10 @@ PulsarLib:Include("core/sh_logging")
 PulsarLib:Include("core/sh_notify")
 
 --[[
-    Dont load PulsarLib (or its addons) below version 2024.04.24. 
+    Dont load PulsarLib (or its addons) below version 2024.03.21. 
     Versions below this will not be able to write .gma files which PulsarLib and its addons REQUIRE to load modules.
 --]]
-if VERSION < 240424 then
+if VERSION < 240321 then
     for i = 1, 10 do
         PulsarLib.Logging:Fatal("PulsarLib is NOT loading due to your servers version being too old. Please update your server.")
     end 
